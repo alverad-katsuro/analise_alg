f9253b3ad9982a21a6ceae64f6479338cfc906f3
Alfredo Gabriel <amgabriel1@hotmail.com>
2022-11-20 22:00:03 -0300

    update: justificativa

--- src/main/java/com/desafio/Escalonador.java
+++ src/main/java/com/desafio/Escalonador.java
@@ -9,12 +9,12 @@ public class Escalonador {
     }
 
     public static int Scheduling(int tempoServico[], int totalCliente) { // Θ(n)
-        Arrays.sort(tempoServico);                      // Θ(nlog(n))
+        Arrays.sort(tempoServico);                      // Θ(nlog(n)) -> Segundo a Oracle QuickSort com 2 pivores.
         int acumulador = 0;                             // Θ(1)
         int tempoTotal = 0;                             // Θ(1)
-        for (int i = 0; i < totalCliente; i++) {        // Θ(n)
-            acumulador += tempoServico[i];              // Θ(n-1)
-            tempoTotal += acumulador;                   // Θ(n-1)
+        for (int i = 0; i < totalCliente; i++) {        // Θ(totalCliente)
+            acumulador += tempoServico[i];              // Θ(totalCliente-1)
+            tempoTotal += acumulador;                   // Θ(totalCliente-1)
         }
         return tempoTotal;                              // Θ(1)
     }
